1f43a4b56a0dba9b249ed6223a9c9fdfc59db6f3
Prashant Goyal <prashantgoyal@Prashants-MacBook-Neo.local>
2026-08-08 14:57:06 +0530

    Fixed a few errors

--- backend/catalog/urls.py
+++ backend/catalog/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProductViewSet
 
 router = DefaultRouter()
-router.register("categories", CategoryViewSet)
-router.register("products", ProductViewSet)
+router.register("categories", CategoryViewSet, basename="category")  # <-- ADDED basename
+router.register("products", ProductViewSet, basename="product")       # <-- ADDED basename
+
 
 urlpatterns = router.urls
