ff5d1dc0fc92c0f21b85acc594545dfb2c25e99d
Prashant Goyal <prashantgoyal@Prashants-MacBook-Neo.local>
2026-08-14 09:59:24 +0530

    Fixed the MTO flow and stock options shown

--- backend/catalog/serializers.py
+++ backend/catalog/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from .models import Category, Product, ProductMedia, ProductInstance
+from .models import Category, Product, ProductMedia, ProductInstance, RateCard
+
+class RateCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RateCard
+        fields = ["gold_rate_14kt", "gold_rate_18kt", "diamond_rate_per_carat", "making_charges_percentage", "gst_percentage"]
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -48,6 +53,7 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     media = ProductMediaSerializer(many=True, read_only=True)
     instances = ProductInstanceSerializer(many=True, read_only=True)
     category_name = serializers.CharField(source="category.name", read_only=True)
+    rate_card = serializers.SerializerMethodField()  # ADD THIS
 
     class Meta:
         model = Product
@@ -56,4 +62,14 @@ class ProductDetailSerializer(serializers.ModelSerializer):
                   "diamond_weight_round_melle", "pointer_solitaire_weight", "fancy_cut_weight",
                   "color_stone_weight", "diamond_color", "diamond_clarity",
                   "has_solitaire_pointer", "has_fancy_cut", "has_color_stone",
-                  "media", "instances"]
+                  "media", "instances", "rate_card"]  # ADD "rate_card" HERE
+
+    def get_rate_card(self, obj):
+        rc = RateCard.get()
+        return {
+            "gold_rate_14kt": float(rc.gold_rate_14kt),
+            "gold_rate_18kt": float(rc.gold_rate_18kt),
+            "diamond_rate_per_carat": float(rc.diamond_rate_per_carat),
+            "making_charges_percentage": float(rc.making_charges_percentage),
+            "gst_percentage": float(rc.gst_percentage),
+        }
