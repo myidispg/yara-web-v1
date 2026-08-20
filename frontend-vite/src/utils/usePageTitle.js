138fba6dac8d1224322f50e3e173b3f9c8c589eb
Prashant Goyal <prashantgoyal@Prashants-MacBook-Neo.local>
2026-08-20 18:05:57 +0530

    Removed frontend-vite folder

--- frontend-vite/src/utils/usePageTitle.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function usePageTitle(title) {
-    useEffect(() => {
-        document.title = title
-            ? `${title} | YA-RA®`
-            : "YA-RA® | Natural Diamond Fine Jewellery";
-    }, [title]);
-}
